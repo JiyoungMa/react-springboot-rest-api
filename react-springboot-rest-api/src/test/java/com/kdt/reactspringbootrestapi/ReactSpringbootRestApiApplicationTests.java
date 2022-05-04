fa8411d648b7b38fb55f366c106cf2c297ad995f
DESKTOP-5836PEC\tiger <tiger980424@naver.com>
2022-05-04 14:18:19 +0900

    초기 프로젝트 설정 완료

---
+++ react-springboot-rest-api/src/test/java/com/kdt/reactspringbootrestapi/ReactSpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.reactspringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
